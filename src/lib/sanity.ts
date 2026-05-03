c5d38b1e5d460b6e9e07dd9836251e73e7de7a3c
karki S <karkisenthilkumar@gmail.com>
2026-05-03 19:24:56 +0530

    fix: hardcode sanity project id for studio stability

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -3,7 +3,7 @@ import { createClient } from 'next-sanity'
 import createImageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
+  projectId: 'dol70uz3',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: '2024-01-01',
   useCdn: true,
